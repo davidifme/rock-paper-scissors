f0fd590e73322880f42dbc58dbbb741206d8d098
David <davidifme@icloud.com>
2024-11-07 17:13:25 +0100

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getComputerChoice() {
     // Generate a random number between 1 and 3.
     let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    let weapon = '';
+
+    if (randomNumber === 1) {
+        weapon = 'rock';
+    } else if (randomNumber === 2) {
+        weapon = 'paper';
+    } else {
+        weapon = 'scissors';
+    }
+
+    return weapon;
 }
