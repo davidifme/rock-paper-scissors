816125c39dc4e7e809dd342c38b83180c1d5f924
David <davidifme@icloud.com>
2024-11-16 18:39:08 +0100

    add funcionality for playing with buttons

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-const selectionButtons = document.querySelectorAll("button.selectionButton");
+const selectionButtons = document.querySelectorAll("button.selection-button");
+const gameDisplay = document.querySelector("#display");
+const roundCounterDisplay = document.querySelector("#round-counter");
+const scoreDisplay = document.querySelector("#score")
 
 selectionButtons.forEach((button) => {
     button.addEventListener("click", () => {
         humanSelection = button.textContent.toLowerCase();
 
-        playRound(humanSelection, getComputerChoice());
+        playGame(humanSelection, getComputerChoice());
     });
 });
 
+let humanChoice = '';
+let humanScore = 0;
+let computerScore = 0;
+let roundCounter = 0;
 
 function getComputerChoice() {
     // Generate a random number between 1 and 3.
@@ -25,59 +32,71 @@ function getComputerChoice() {
     return weapon;
 }
 
-function playRound(humanChoice, computerChoice){
-    const gameDisplay = document.querySelector("#display");
+function playGame (humanChoice, computerChoice) {
 
-    switch(humanChoice) {
-        case 'rock':
-            if (computerChoice === 'rock') {
-                gameDisplay.textContent = "It's a draw!";
+    let playRound = () => {
+        switch(humanChoice) {
+            case 'rock':
+                if (computerChoice === 'rock') {
+                    gameDisplay.textContent = "It's a draw!";
+                    break;
+                } else if (computerChoice !== 'paper') {
+                    humanScore++;
+                    computerScore--;
+                    gameDisplay.textContent = "Rock beats Scissors - You win!";
+                    break;
+                } else {
+                    humanScore--;
+                    computerScore++;
+                    gameDisplay.textContent = "Paper beats Rock - You lose!";
+                    break;
+                }
+            case 'paper':
+                if (computerChoice === 'paper') {
+                    gameDisplay.textContent = "It's a draw!";
+                    break;
+                } else if (computerChoice !== 'scissors') {
+                    humanScore++;
+                    computerScore--;
+                    gameDisplay.textContent = "Paper beats Rock - You win!";
+                    break;
+                } else {
+                    humanScore--;
+                    computerScore++;
+                    gameDisplay.textContent = "Scissors beats Paper - You lose!";
+                    break;
+                }
+            case 'scissors':
+                if (computerChoice === 'scissors') {
+                    gameDisplay.textContent = "It's a draw!";
+                    break;
+                } else if (computerChoice !== 'rock') {
+                    humanScore++;
+                    computerScore--;
+                    gameDisplay.textContent = "Scissors beats Paper - You win!";
+                    break;
+                } else {
+                    humanScore--;
+                    computerScore++;
+                    gameDisplay.textContent = "Rock beats Scissors - You lose!";
+                    break;
+                }
+            default:
+                gameDisplay.textContent = "Invalid weapon";
                 break;
-            } else if (computerChoice !== 'paper') {
-                humanScore++;
-                computerScore--;
-                gameDisplay.textContent = "Rock beats Scissors - You win!";
-                break;
-            } else {
-                humanScore--;
-                computerScore++;
-                gameDisplay.textContent = "Paper beats Rock - You lose!";
-                break;
-            }
-        case 'paper':
-            if (computerChoice === 'paper') {
-                gameDisplay.textContent = "It's a draw!";
-                break;
-            } else if (computerChoice !== 'scissors') {
-                humanScore++;
-                computerScore--;
-                gameDisplay.textContent = "Paper beats Rock - You win!";
-                break;
-            } else {
-                humanScore--;
-                computerScore++;
-                gameDisplay.textContent = "Scissors beats Paper - You lose!";
-                break;
-            }
-        case 'scissors':
-            if (computerChoice === 'scissors') {
-                gameDisplay.textContent = "It's a draw!";
-                break;
-            } else if (computerChoice !== 'rock') {
-                humanScore++;
-                computerScore--;
-                gameDisplay.textContent = "Scissors beats Paper - You win!";
-                break;
-            } else {
-                humanScore--;
-                computerScore++;
-                gameDisplay.textContent = "Rock beats Scissors - You lose!";
-                break;
-            }
-        default:
-            gameDisplay.textContent = "Invalid weapon";
-            break;
+        }
+        scoreDisplay.textContent = humanScore;
+    };
+
+    if (roundCounter < 5) {
+        roundCounter++;
+        roundCounterDisplay.textContent = `${roundCounter}`
+        playRound();
+    } else if (roundCounter === 5){
+        roundCounterDisplay.textContent = '1';
+        humanScore = 0;
+        computerScore = 0;
+        roundCounter = 1;
+        playRound();
     }
-
-    gameDisplay.textContent = '';
-};
+}
